f75f32e6e7c770aba4dc0e65add793ba63af6795
Jon Woodring <woodring@lanl.gov>
2017-08-11 16:06:59 -0600

    Checker for Spec D.

--- clib/spec/d/__impl__.py
+++ clib/spec/d/__impl__.py
@@ -1,206 +1,246 @@
+"""
+Specification D functions and utilities for reading and validating databases.
+"""
+
 import os
 import logging as log
 import csv
-from ... import spec
+from functools import reduce
 
-SPECD_CSV_FILENAME = "data.csv"
+SPEC_D_CSV_FILENAME = "data.csv"
+FILE_HEADER_KEYWORD = "FILE"
+TYPE_INTEGER = "INTEGER"
+TYPE_FLOAT = "FLOAT"
+TYPE_STRING = "STRING"
 
-def get_csv_reader(db, csv=SPECD_CSV_FILENAME):
+## TODO REMOVE
+log.basicConfig(format='%(asctime)s %(levelname)s:%(message)s', level=log.DEBUG, datefmt='%I:%M:%S')
+
+def get_csv_reader(db_path, csv_path=SPEC_D_CSV_FILENAME):
     """
     Return a csv reader, assuming a valid Spec D csv database.
 
     arguments:
-        db : string
+        db_path : string
             POSIX path to Cinema database
-        csv : string = SPECD_CSV_FILENAME
+        csv_path : string = SPEC_D_CSV_FILENAME
             POSIX relative path to Cinema CSV
+
+    returns:
+        csv.reader that is open to the Cinema CSV if the csv_path file
+        can be opened, otherwise returns None
     """
 
     reader = None
-    csv = os.path.join(db, csv)
+    fn = os.path.join(db_path, csv_path)
 
-    if os.path.isfile(csv):
-        f = open(csv, "r") 
+    if os.path.isfile(fn):
+        f = open(fn, "r") 
         reader = csv.reader(f, delimiter=",", doublequote=False, 
                  escapechar=None)
-    else:
-        log.warning("csvfile {0} does not exist", format(csvfile))
 
     return reader
 
-class CinemaSpecD(spec.CinemaSpec):
-    CSV_FILENAME = "data.csv"
+def csv_next(csv_reader):
+    return [col.strip() for col in next(csv_reader)]
 
-    def __init__(self):
-        self.dbspec    = "N"
-        self.dbversion = "0.0"
+def csv_iterator(csv_reader):
+    return ([col.strip() for col in row] for row in csv_reader)
 
-    def __del__(self):
-        return
+def typecheck(values):
+    """
+    Return a list of Spec D types given a list of strings.
 
-    # -----------------------------------------------------------------------
-    #
-    # return a csv reader w/correct grammar, delimiter, escape characters 
-    #
-    # -----------------------------------------------------------------------
-    def __get_csvreader(self, csvfile ):
-        reader = None
+    arguments:
+        values : list of strings
+    
+    returns:
+        list of types (TYPE_INTEGER, TYPE_FLOAT, TYPE_STRING, or TYPE_FILE)
+    """
 
-        if os.path.isfile(csvfile):
-            openfile = open(csvfile, "r") 
-            reader = csv.reader( openfile, delimiter=",", 
-                           doublequote=False, escapechar='\\')
-        else:
-            log.warning("csvfile {0} does not exist",format(csvfile))
+    types = []
 
-        return reader
-
-    # -----------------------------------------------------------------------
-    #
-    # check a database D v1.0 
-    #
-    # -----------------------------------------------------------------------
-    def check_database(self, dbname ):
-        res = True
-
-        log.info("checking database {0} as Spec D".format(dbname))
-        if os.path.isdir( dbname ):
-            # does the csv file exist
-            reader = self.__get_csvreader(
-                        os.path.join(dbname,self.CSV_FILENAME))
-
-            if reader != None:
-                numfields = 0
-                isFirstLine = True
-                hasFileField = False
-                for row in reader: 
-                    if isFirstLine:
-                        isFirstLine = False
-                        numfields = len( row )
-                        if row[-1] == "FILE":
-                            hasFileField = True
-                    else:
-                        if len(row) == numfields:
-                            if hasFileField: 
-                                if not os.path.isfile(os.path.join(dbname, row[-1])):
-                                    log.error("ERROR: File does not exist: {0}".format(row[-1]))
-                                    res = False
-                        else:
-                            res = False 
-
-
-            else:
-                res = False
-                log.error("required file {0} does not exist".format(CSV_FILENAME))
-        else:
-            res = False
-            log.error("database {0} does not exist".format( dbname ))
-
-        if res:
-            log.info("check succeeded: valid D database")
-
-        return res
-        
-
-    # -----------------------------------------------------------------------
-    #
-    # convert Spec A to Spec D 
-    #
-    # -----------------------------------------------------------------------
-
-    # ----------------------------------------------------------------------
-    #
-    # Recursively generate results for all possible combinations of parameters
-    # Starts from the parameter at index, and uses values for the value of     
-    # earlier parameters
-    #
-    # generate_results(0,{}) will generate ALL results for ALL parameters
-    #
-    # ----------------------------------------------------------------------
-    def __generate_results(self, index, values, fieldnames, inputJson):
-        results = []
-        for val in inputJson["arguments"][fieldnames[index]]["values"]:
-            values[fieldnames[index]] = val
-            if index == len(fieldnames)-1:
-                # print values
-                results.append(copy.copy(values))
-            else:
-                results += (this.__generate_results(index+1,
-                                copy.copy(values),fieldnames, inputJson))
-        return results
-
-
-    # ----------------------------------------------------------------------
-    #
-    #
-    #
-    # ----------------------------------------------------------------------
-    def convert_atod(self, inputDB, newDB): 
-        # check that the new DB path is valid and doesn't exist
-        newDB = newDB.rstrip("/")
-        baseFile, extension = os.path.splitext(newDB)
-        if extension != ".cbd":
-            # add cdb extension
-            newDB = newDB + ".cdb"
-
-        # check output path and create; exit if error
-        if os.path.isdir( newDB ):
-            log.warning("Output database {0} already exists.".format(newDB))
-            exit(1)
-
-        log.info("Creating new Cinema database at {0}".format(newDB))
-
+    for v in values:
         try:
-        	os.mkdir(OUTPUT_DIRECTORY)
-        except OSError as exc:
-        	if exc.errno != errno.EEXIST:
-        		raise
-        	pass
+            test = int(v)
+            types.append(TYPE_INTEGER)
+        except:
+            try:
+                test = float(v.lower())
+                types.append(TYPE_FLOAT)
+            except:
+                types.append(TYPE_STRING)
+    return types
+
+def check_database(db_path, csv_path=SPEC_D_CSV_FILENAME):
+    """
+    Validate a Spec D database.
+
+    arguments:
+        db_path : string
+            POSIX path to Cinema database
+        csv_path : string = SPEC_D_CSV_FILENAME
+            POSIX relative path to Cinema CSV
+
+    returns:
+        True if it is valid, False otherwise
+
+    side effects:
+        logs error and info messages to the logger
+    """
+
+    res = True
+
+    log.info("Checking database \"{0}\" as Spec D.".format(db_path))
+    try:
+        # get the reader
+        reader = get_csv_reader(db_path, csv_path)
+        if reader == None:
+            log.error("CSV \"{0}\" does not exist.".format(csv_path))
+            raise 
+
+        # read the header
+        header = csv_next(reader)
+        log.info("Header is {0}.".format(header))
+        columns = len(header)
+        log.info("Number of columns are {0}.".format(len(header)))
+
+        # read the first line and types
+        row = csv_next(reader)
+        log.info("First data row is {0}.".format(row))
+        types = typecheck(row)
+        log.info("Data types are {0}.".format(types))
+        files = [i for i, t, h in zip(range(0, len(types)), types, header) if
+                 h == FILE_HEADER_KEYWORD and t == TYPE_STRING]
+        log.info("FILE column indices are {0}.".format(files))
+
+        # reopen the reader because we are lazy and skip the header
+        reader = get_csv_reader(db_path, csv_path)
+        csv_next(reader)
+
+        # check the rows
+        row_error = False
+        n_rows = 0
+        n_files = 0
+        for row in csv_iterator(reader):
+            n_rows = n_rows + 1
+            if len(header) != len(row):
+                log.error(
+                  "Unequal number of columns on row #{0}.".format(n_rows))
+                row_error = True
+            if not reduce(lambda x, y: x and y, 
+                          [a == b for a, b in zip(typecheck(row), types)],
+                          True):
+              log.error("Types do not match on row #{0}:".format(n_rows))
+              row_error = True
+            # check the files
+            for i in files:
+                fn = os.path.join(db_path, row[i])
+                if not os.path.exists(fn):
+                    log.error("File \"{0}\" is missing.".format(fn))
+                    row_error = True
+                else:
+                    n_files = n_files + 1
+
+        if row_error:
+            log.warning("Only {0} files were found.".format(n_files))
+            raise
+        else:
+            log.info("{0} files validated to be present.".format(n_files))
+            log.info("Number of rows are {0}.".format(n_rows))
+    except:
+        log.error("Check failed. \"{0}\" is invalid.".format(db_path))
+        return False
+
+    log.info("Check succeeded.")
+    return True
+
+#### TODO To fix
+
+def __generate_results(self, index, values, fieldnames, inputJson):
+    results = []
+    for val in inputJson["arguments"][fieldnames[index]]["values"]:
+        values[fieldnames[index]] = val
+        if index == len(fieldnames)-1:
+            # print values
+            results.append(copy.copy(values))
+        else:
+            results += (this.__generate_results(index+1,
+                            copy.copy(values),fieldnames, inputJson))
+    return results
+
+
+# ----------------------------------------------------------------------
+#
+#
+#
+# ----------------------------------------------------------------------
+def convert_atod(self, inputDB, newDB): 
+    # check that the new DB path is valid and doesn't exist
+    newDB = newDB.rstrip("/")
+    baseFile, extension = os.path.splitext(newDB)
+    if extension != ".cbd":
+        # add cdb extension
+        newDB = newDB + ".cdb"
+
+    # check output path and create; exit if error
+    if os.path.isdir( newDB ):
+        log.warning("Output database {0} already exists.".format(newDB))
+        exit(1)
+
+    log.info("Creating new Cinema database at {0}".format(newDB))
+
+    try:
+            os.mkdir(OUTPUT_DIRECTORY)
+    except OSError as exc:
+            if exc.errno != errno.EEXIST:
+                    raise
+            pass
+    
+    jsonFile = os.path.join(inputDB, "image", "info.json") 
+    inputJson = None
+    with open(jsonFile) as f:
+        inputJson = json.load(f)
+    
+        # get fieldnames (parameters) from JSON
+        fieldnames = []
+        for arg in inputJson["arguments"]:
+            fieldnames.append(str(arg))
         
-        jsonFile = os.path.join(inputDB, "image", "info.json") 
-        inputJson = None
-        with open(jsonFile) as f:
-            inputJson = json.load(f)
-        
-            # get fieldnames (parameters) from JSON
-            fieldnames = []
-            for arg in inputJson["arguments"]:
-                fieldnames.append(str(arg))
+        # Write csv and copy images
+        csvFileName = os.path.join( newDB, 'data.csv')
+        with open(csvFileName, 'w+') as csvfile:
+            namePattern = str(inputJson["name_pattern"])
+            newExt = os.path.splitext(namePattern)[1]
             
-            # Write csv and copy images
-            csvFileName = os.path.join( newDB, 'data.csv')
-            with open(csvFileName, 'w+') as csvfile:
-                namePattern = str(inputJson["name_pattern"])
-                newExt = os.path.splitext(namePattern)[1]
-                
-                # get results
-                values = {}
-                results = this.__generate_results(0, {}, fieldnames, inputJson)
-                
-                # we have to add one more fieldname, specific to the conversion
-                fieldnames.append("FILE")
-                writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-                writer.writeheader()
-                newFileID = 0
-                for result in results:
-                    pattern = copy.copy(namePattern)
-                    for dimension in result.iterkeys():
-                        pattern = pattern.replace('{'+dimension+'}',
-                                    str(result[dimension]))
-                    # If an image for the result exists, copy it to output
-                    # and write a line to the csv
-                    if os.path.exists(os.path.join(inputDB, pattern)):
-                        src = os.path.join(inputDB, pattern)
-                        newFileID += 1
-                        newFileName = str(newFileID)+str(newExt)
-                        dst = os.path.join(newDB, newFileName) 
-                        copyfile(src,dst)
-                        result["FILE"] = newFileName
-                        writer.writerow(result)
-                log.info("Done converting database")
+            # get results
+            values = {}
+            results = this.__generate_results(0, {}, fieldnames, inputJson)
+            
+            # we have to add one more fieldname, specific to the conversion
+            fieldnames.append("FILE")
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+            newFileID = 0
+            for result in results:
+                pattern = copy.copy(namePattern)
+                for dimension in result.iterkeys():
+                    pattern = pattern.replace('{'+dimension+'}',
+                                str(result[dimension]))
+                # If an image for the result exists, copy it to output
+                # and write a line to the csv
+                if os.path.exists(os.path.join(inputDB, pattern)):
+                    src = os.path.join(inputDB, pattern)
+                    newFileID += 1
+                    newFileName = str(newFileID)+str(newExt)
+                    dst = os.path.join(newDB, newFileName) 
+                    copyfile(src,dst)
+                    result["FILE"] = newFileName
+                    writer.writerow(result)
+            log.info("Done converting database")
 
 #           except IOError:
 #               log.warning("Cannot open csv file {0}".format(csvFileName))
-                 
+             
 #       except IOError:
 #           log.warning("Cannot open json file {0}".format(jsonFile))
