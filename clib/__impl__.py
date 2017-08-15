6dcd88ff2a1c65a446c43b8e9e7197c124afd477
Jon Woodring <woodring@lanl.gov>
2017-08-15 10:56:08 -0600

    Added a small bit of help.

--- clib/__impl__.py
+++ clib/__impl__.py
@@ -1,3 +1,8 @@
 """
 Utility library for Cinema databases.
+
+Importing this will import all the sub-modules which are currently:
+    cinema.spec: utilities for Specifications
+    cinema.spec.a: utilities for Spec A
+    cinema.spec.d: utilities for Spec D
 """
